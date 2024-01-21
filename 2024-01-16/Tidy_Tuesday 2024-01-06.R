8ee6a49605a1dc39d70f8bdfb0720b77a2e591f0
Gregory Vander Vinne <g.vandervinne1@gmail.com>
2024-01-20 17:04:02 -0700

    Complete except messed up ggtext spacing

--- 2024-01-16/Tidy_Tuesday 2024-01-06.R
+++ 2024-01-16/Tidy_Tuesday 2024-01-06.R
@@ -15,7 +15,35 @@ pacman::p_load(
 
 ######## Load and Wrangle Data ########
 
-# Read data sets directly from github
+# Read data set directly from github
+myData <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-01-16/polling_places.csv')
+
+# # View election dates in data
+# myData %>% 
+#   group_by(election_date) %>%
+#   summarise(Count = n())
+
+#Get number of polling places in 2016 and 2020 for states with data in both years
+myData <- myData %>%
+  filter(as.character(election_date) %in% c("2016-11-08","2020-11-03")) %>%
+  mutate(year = year(election_date)) %>%
+  group_by(state, year) %>%
+  summarise(n_places = n()) %>%
+  pivot_wider(values_from = n_places, names_from = year) %>%
+  mutate(change = `2020`-`2016`, latest = `2020`) %>%
+  drop_na()
+
+#Get mean across states
+mean_places <- colMeans(myData[,-1]) %>%
+  t() %>%
+  as.data.frame() %>%
+  mutate(state = "TOT")
+
+#Combine with myData and add dummy indicating overall mean
+myData <- rbind(myData, mean_places) %>%
+  mutate(mean = if_else(state == "TOT", TRUE, FALSE)) %>% # must have data for both years
+  pivot_longer(cols = c(`2016`,`2020`),
+               names_to = "year", values_to = "n_places")
 
 
 
@@ -23,5 +51,93 @@ pacman::p_load(
 
 # Save color palette that looks like RStudio's cobalt
 
-myPal <- c("#D48902", "#002141", "#D2D8DD", "#1FA40E", "#A44A70", "#006BD1")
+myPal <- c("#D48902","#1FA40E", "#A44A70", "#006BD1", "#4A5764")
+myCaption <- c("Data: The Center for Public Integrity", "Created by Gregory Vander Vinne")
+
+back_colour = "#002141"
+line_colour = "#193753"
+strong_text = "#D2D8DD"
+weak_text = darken(strong_text, 0.2)
+
+
+#Add Roboto font family
+font_add(family = "Roboto", 
+         regular = "C:/USERS/GVAND/APPDATA/LOCAL/MICROSOFT/WINDOWS/FONTS/ROBOTO-REGULAR.ttf",
+         bold = "C:/USERS/GVAND/APPDATA/LOCAL/MICROSOFT/WINDOWS/FONTS/ROBOTO-BOLD.ttf")
+showtext_auto()
+
+# Define Theme
+my_theme <- function(base_size = 10) {
+  theme_classic(base_size = base_size)+
+    theme(
+      axis.line = element_blank(),
+      panel.grid.major.y = element_line(color = line_colour),
+      axis.ticks = element_blank(),
+      legend.position = c(0.065,1.04),
+      legend.direction = "horizontal",
+      legend.margin = margin(8, 0, 8, 0),
+      legend.background = element_rect(fill = back_colour),
+      panel.background = element_rect(fill = back_colour),
+      plot.background = element_rect(fill = back_colour),
+      plot.caption.position = "plot",
+      plot.title.position = "plot",
+      plot.title = element_textbox_simple(size = rel(1.75),
+                                          family = "Roboto",
+                                          face = "bold",
+                                          color = strong_text,
+                                          margin = margin(5, 0, 10, 0)),
+      plot.subtitle = element_textbox_simple(size = rel(1.1),
+                                             family = "Roboto",
+                                             colour = strong_text,
+                                             margin = margin(0, 0, 28, 0)), 
+      axis.title = element_blank(),
+      axis.text = element_text(size = rel(0.9),
+                               family = "Roboto",
+                               colour = weak_text),
+      plot.caption = element_text(size = rel(0.8),
+                                  colour = weak_text,
+                                  hjust = c(0,1)),
+      legend.text = element_text(size = rel(1),
+                                 family = "Roboto",
+                                 colour = strong_text),
+      legend.title = element_text(size = rel(1),
+                                  family = "Roboto",
+                                  colour = weak_text),
+      text = element_text(colour = weak_text, lineheight = 1.1), 
+      strip.background = element_blank(), 
+      strip.placement = "outside", 
+      strip.text = element_text(size = rel(1),
+                                family = "Roboto",
+                                colour = weak_text)
+    )
+}
+
+# A Dumbbell Plot
+
+ggplot(myData, aes(x = reorder(state,latest), y = n_places)) + 
+  geom_line(color = myPal[4], alpha = 0.5, linewidth = 3.5) +  # we want thick line
+  geom_point(aes(color = year, shape = mean), size = 3.5) +  # Big dots
+  coord_flip() +
+  scale_color_manual(values = myPal, name = "Election Year:") +
+  scale_shape_manual(values = c(16,15), guide = "none") +
+  ggtitle(paste("Number of Polling Places by State in ",
+                     "<span style='color:", myPal[1], "'>2016 </span>", 
+                     "and", 
+                     "<span style='color:", myPal[2], "'>2020 </span>"), 
+          subtitle = "This dumbbell plot shows the number of polling places in
+                        states for which data are available for the last two 
+                        American presidential elections. Overall, the number of 
+                        polling places was slightly down in these states. The 
+                        overall decline was driven by large decreases Minnesota, 
+                        Maryland, and Indiana.") +
+  my_theme()
+
+
+
+
+
+
+
+
+
 
