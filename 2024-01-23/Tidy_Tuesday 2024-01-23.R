370119fff441b110801acec2214d5e780cffe060
Gregory Vander Vinne <g.vandervinne1@gmail.com>
2024-01-28 14:48:51 -0700

    spelling

--- 2024-01-23/Tidy_Tuesday 2024-01-23.R
+++ 2024-01-23/Tidy_Tuesday 2024-01-23.R
@@ -125,13 +125,13 @@ ggplot(myData, aes(x = income_flag, y = education_score,
        England. The towns are grouped according to their income deprivation category
        along the x-axis, while the towns education scores fo towns are
        given on the y-axis. The width of each swarm at any given point on the 
-       y-axis illustrates how many towns in that income deprevation category 
+       y-axis illustrates how many towns in that income deprivation category 
        have educational scores at that level. The marginal density
        plot on the side shows the distribution of education scores 
        for coastal and non-coastal towns. We see that
-       towns with higher levels of income deprevation (poorer towns) tend to have
+       towns with higher levels of income deprivation (poorer towns) tend to have
        lower educational acheivement scores and vice versa. Furthermore, coastal 
-       towns are more likely to be in the higher income deprevation category and
+       towns are more likely to be in the higher income deprivation category and
        tend to have lower educational scores.",
        caption = myCaption) + 
   ylab("Education Score") + 
