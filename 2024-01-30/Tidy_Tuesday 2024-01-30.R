51faa715ad733057bb66612bc2fcf54db955b725
Gregory Vander Vinne <g.vandervinne1@gmail.com>
2024-02-01 21:37:37 -0700

    Completed

--- 2024-01-30/Tidy_Tuesday 2024-01-30.R
+++ 2024-01-30/Tidy_Tuesday 2024-01-30.R
@@ -11,6 +11,8 @@ pacman::p_load(
   ggtext,         # fancy text in plots
   colorspace,     # fancy stuff with colors
   rnaturalearth,  # to get province/state borders for Canada/USA
+  sf,             # GIS stuff
+  RColorBrewer,   # color palettes
   glue            # text concatanation w/ formatting
 )
 
@@ -34,29 +36,26 @@ myData <- predictions %>%
   left_join(groundhogs, by = "id")
 
 # Get borders of Canada and the USA
-state_prov <- rnaturalearth::ne_states(c("united states of america", "canada")) %>%
+borders <- rnaturalearth::ne_states(c("united states of america", "canada")) %>%
   filter(!(name %in% c("Hawaii", "Alaska", "Northwest Territories", "Nunavut", 
                        "Yukon")))
 
-ggplot() + 
-  geom_sf(state_prov) + 
-  geom_point(myData, aes(x = longitude, y = latitude))
+#Filter to just Punxsutawney Phil
+phil <- myData %>%
+  filter(name == "Punxsutawney Phil")
+
 
 ######## Set Up For Plotting ########
 
 # Set up a color scheme
-myPal <- c("#BF40BF", "grey")
-myCaption <- glue("<b> Data: </b> Groundhog-Day.com API",
-                  "  \n <b> Graphic: </b> Gregory Vander Vinne")
-
-back_colour = lighten("#EFEFEF",0.05)
-line_colour = "grey20"
-strong_text = "black"
+myPal <- brewer.pal(8, "RdBu")[c(1,8)]
+back_colour = lighten("#f2e86d", 0.95)
+strong_text = darken("#696047",0.75)
 weak_text = lighten(strong_text, 0.2)
 
 
 # Add custom font family
-font_add_google("Roboto", "roboto")
+font_add_google("Cabin", "cabin")
 showtext_auto()
 
 
@@ -65,46 +64,35 @@ my_theme <- function(base_size = 10) {
   theme_classic(base_size = base_size)+
     theme(
       axis.line = element_blank(),
-      panel.grid.major.y = element_line(color = line_colour),
       axis.ticks = element_blank(),
-      legend.position = c(0.1,1.025),
-      legend.direction = "horizontal",
-      legend.margin = margin(8, 0, 8, 0),
-      legend.background = element_rect(fill = back_colour),
+      # legend.position = c(0.99,0.25),
+      # legend.margin = margin(0, 0, 0, 0),
+      # legend.background = element_rect(fill = back_colour),
+      legend.position = "none",
       panel.background = element_rect(fill = back_colour),
       plot.background = element_rect(fill = back_colour),
       plot.caption.position = "plot",
       plot.title.position = "plot",
       plot.title = element_textbox_simple(size = rel(1.75),
-                                          family = "Roboto",
-                                          face = "bold",
+                                          family = "Cabin",
+                                          # face = "bold",
                                           color = strong_text,
                                           margin = margin(5, 0, 10, 0)),
       plot.subtitle = element_textbox_simple(size = rel(1.1),
-                                             family = "Roboto",
+                                             family = "Cabin",
                                              colour = weak_text,
                                              margin = margin(0, 0, 28, 0)), 
-      axis.title.x = element_text(size = rel(1.1),
-                                  family = "Roboto",
-                                  colour = strong_text, 
-                                  margin = margin(16, 0, 4, 0), 
-                                  hjust = adjust),
-      axis.title.y = element_text(size = rel(1.1),
-                                  family = "Roboto",
-                                  colour = strong_text, 
-                                  margin = margin(0, 6, 0, 2)),
-      axis.text= element_text(size = rel(0.9),
-                              family = "Roboto",
-                              colour = weak_text),
+      axis.title = element_blank(),
+      axis.text= element_blank(),
       plot.caption = element_markdown(size = rel(0.8),
                                       colour = weak_text,
-                                      family = "Roboto",
+                                      family = "Cabin",
                                       hjust = 0),
       legend.text = element_text(size = rel(1),
-                                 family = "Roboto",
+                                 family = "Cabin",
                                  colour = strong_text),
       legend.title = element_text(size = rel(1),
-                                  family = "Roboto",
+                                  family = "Cabin",
                                   colour = weak_text),
       text = element_text(colour = weak_text, lineheight = 1.1)
     )
@@ -112,5 +100,61 @@ my_theme <- function(base_size = 10) {
 
 
 ######## The Actual Plot ########
+
+#Define some of my text
+myCaption <- glue("<b> Data: </b> Groundhog-Day.com",
+                  "  \n <b> Graphic: </b> Gregory Vander Vinne")
+
+myAnnotation <- paste0(phil$name, " has made ", 
+                      "\n",
+                      phil$predictions, " predictions - the most of any",
+                      "\n",
+                      " groundhog. He predicts a longer", 
+                      "\n",
+                      " winter ", round(phil$share_shadow*100,1), "% of the time.")
+
+#Plot
+ggplot() + 
+  geom_sf(data = borders, fill = back_colour, color = "grey70") + 
+  geom_point(data = myData, aes(x = longitude, y = latitude, 
+                                color = share_shadow, size = predictions), 
+             alpha = 0.75) + 
+  geom_text(
+    data = data.frame(
+      x = -67,
+      y = 36,
+      label = c(myAnnotation)
+    ),
+    mapping = aes(x = x, y = y, label = myAnnotation),
+    family = "Cabin",
+    colour = weak_text,
+    size = 3,
+    lineheight = 0.85
+  ) +
+  geom_curve(
+    data = data.frame(x2 = phil$longitude,
+                      x1 = -73,
+                      y2 = phil$latitude-0.35,
+                      y1 = 37),
+    mapping = aes(x = x1, y = y1, xend = x2, yend = y2),
+    arrow = arrow(length = unit(0.03, "npc")),
+    colour = lighten(weak_text,0.2),
+    curvature = -0.2
+  ) +
+  labs(title = paste("Which Groundhogs Tend to Predict", 
+                     "<span style='color:", myPal[1], "'>More Winter</span>", 
+                     "Versus an", 
+                     "<span style='color:", myPal[2], "'>Early Spring </span>"), 
+       subtitle = "On this map, each dot represents a groundhog that makes predictions on Groundhog Day. 
+       Bigger dots represent groundhogs that have been making predictions for more years. 
+       Dots that are more red represent groundhogs that predict longer winters a higher share of the time.
+       Dots that are more blue represent groundhogs that predict early springs a higher share of the time. 
+       A completely grey dot would represent a groundhog that predicts each outcome evenly.", 
+       caption = myCaption) + 
+  scale_y_continuous(limits = c(30,52)) +
+  scale_x_continuous(limits = c(-122, -63)) +
+  scale_color_gradient2(low = myPal[2],  high = myPal[1],
+                       midpoint = 0.5, mid = "grey45") + 
+  my_theme()
 
 
