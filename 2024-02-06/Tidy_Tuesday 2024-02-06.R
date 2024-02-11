80bb2ec9c5c6e1f58108bb5fd22e4a95cd086fe7
Gregory Vander Vinne <g.vandervinne1@gmail.com>
2024-02-10 22:53:31 -0700

    Completed staircase plot

--- 2024-02-06/Tidy_Tuesday 2024-02-06.R
+++ 2024-02-06/Tidy_Tuesday 2024-02-06.R
@@ -1,6 +1,7 @@
 # Clear memory
 rm(list = ls(all=T))
 
+devtools::install_github("jimjam-slam/ggflags")
 
 # Load packages and install if not installed
 if (!require("pacman")) install.packages("pacman")
@@ -11,7 +12,8 @@ pacman::p_load(
   ggtext,         # fancy text in plots
   colorspace,     # fancy stuff with colors
   glue,           # text concatenation w/ formatting
-  ggalluvial     # flows between bars etc
+  ggalluvial,     # flows between bars etc, 
+  ggflags         # points that look like flags
 )
 
 
@@ -19,7 +21,8 @@ pacman::p_load(
 
 # Read Tidy Tuesday data set directly from github
 myData_wide <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-02-06/heritage.csv')
-myData_wide$abv <- c("NO", "DK", "SE")
+myData_wide <- myData_wide %>% 
+  mutate(abv = c("NO", "DK", "SE"), chg = `2022`-`2004`)
 
 # Re-shape the data and get year-specific rank f/ number of sites
 myData_long <- myData_wide %>%
@@ -36,6 +39,7 @@ myData_long <- myData_wide %>%
 
 # Set up a color scheme
 myPal <- c( "#EE5440", "#283250","#3274D8")
+flagPal <-  c("#ED4C5C","#418BC1","#FFE62E" )
 back_colour = "white"
 line_colour = "#D7DDE1"
 weak_text = "#7A8092"
@@ -43,7 +47,7 @@ strong_text = darken(weak_text, 0.5)
 
 
 # Add custom font family
-font_add_google("Cabin", "cabin")
+font_add_google("Roboto", "roboto")
 showtext_auto()
 
 
@@ -57,12 +61,12 @@ my_theme <- function(base_size = 24) {
       panel.background = element_rect(fill = back_colour, color = back_colour),
       plot.background = element_rect(fill = back_colour),
       plot.title = element_textbox_simple(size = rel(2.25),
-                                          family = "Cabin",
+                                          family = "roboto",
                                           # face = "bold",
                                           color = strong_text,
                                           margin = margin(5, 0, 8, 0)),
       plot.subtitle = element_textbox_simple(size = rel(1.1),
-                                             family = "Cabin",
+                                             family = "roboto",
                                              colour = weak_text,
                                              margin = margin(0, 0, 20, 0), 
                                              lineheight=0.4),
@@ -71,19 +75,19 @@ my_theme <- function(base_size = 24) {
       axis.text= element_blank(),
       plot.caption = element_markdown(size = rel(0.8),
                                       colour = weak_text,
-                                      family = "Cabin",
+                                      family = "roboto",
                                       hjust = 0),
       legend.text = element_text(size = rel(1),
-                                 family = "Cabin",
+                                 family = "roboto",
                                  colour = strong_text),
       legend.title = element_text(size = rel(1),
-                                  family = "Cabin",
+                                  family = "roboto",
                                   colour = weak_text),
       text = element_text(colour = weak_text, lineheight = 0.5)
     )
 }
 
-######## The Actual Plot ########
+######## The Alluvial Plot ########
 
 ggplot(myData_long, 
        aes(x = year, y = sites,
@@ -112,28 +116,59 @@ ggplot(myData_long,
     ),
     inherit.aes = FALSE,
     mapping = aes(x = x, y = y, label = x),
-    family = "Cabin",
+    family = "roboto",
     colour = strong_text,
     size = 6.5*2.4,
     lineheight = 0.3
   ) +
   geom_text(inherit.aes = FALSE,
             aes(y = label_height, label = if_else(year == 2022, abv, "")), x = 2020,
-            family = "Cabin",
+            family = "roboto",
             colour = "white",
             size = 6*2.4) + 
   geom_text(inherit.aes = FALSE,
-            aes(y = label_height, x = if_else(year == min(year), year-0.8, year+0.8),
+            aes(y = label_height, x = if_else(year == min(year), year-0.7, year+0.7),
                 label = sites),
-            family = "Cabin",
+            family = "roboto",
             colour = weak_text,
             size = 6*2.4) + 
   scale_fill_manual(values = myPal) +
   scale_x_continuous(limits = c(2002,2024)) +
-  labs(title = "Number of Unesco World Heritage Sites in Denmark, Norway, & Sweden", 
+  labs(title = "Number of Unesco World Heritage Sites in Denmark, Norway, and Sweden", 
        caption = glue("<b> Data: </b> Unesco.org",
                       "  \n <b> Graphic: </b> Gregory Vander Vinne")) + 
   my_theme()
 
-ggsave(here("2024-02-06/2024-02-06.png"))
+ggsave(here("2024-02-06/2024-02-06_alluvial.png"))
+
+
+######## The ?Staircase? Plot ########
+
+steps <- data.frame(steps = seq(0.5,18, by = 1), 
+                    numbers  = seq(1,18, by = 1))
+
+ggplot() + 
+  geom_flag(data = myData_long, aes(x = sites, y = sites, 
+                                    country = tolower(abv)),
+            size = 5.7) + 
+  geom_curve(data = myData_wide, 
+             aes(x = `2004`, xend = `2022`-0.3,  
+                 y = `2004` + 0.75, yend = `2022` + 0.75, 
+                 color = country), 
+             curvature = -0.75, 
+             angle = 60,
+             arrow = arrow(length = unit(0.03, "npc")),
+             linewidth = 0.7) +
+  geom_step(data = steps, aes(x = steps, y=steps), color = strong_text) +
+  geom_text(data = steps, aes(x = numbers, y = numbers-1.25, label =  numbers),
+            fontface = if_else(steps$numbers %in% myData_wide$`2022`, "bold", "plain"),
+            color = strong_text, size = 6*2.4, family = "Roboto") + 
+  labs(title = "Number of Unesco World Heritage Sites in 2004 and 2022",
+       caption = glue("<b> Data: </b> Unesco.org",
+                      "  \n <b> Graphic: </b> Gregory Vander Vinne")) + 
+  scale_color_manual(values = flagPal) +
+  my_theme()
+
+
+ggsave(here("2024-02-06/2024-02-06_staircase.png"))
 
